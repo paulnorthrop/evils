798279c50b98801cf128ec9d7893d2fef47d5f27
Paul Northrop <p.northrop@ucl.ac.uk>
2023-12-26 15:50:42 +0000

    Check log1pdx2()

---
+++ tests/testthat/test-log1pdx2.R
@@ -0,0 +1,100 @@
+# Check that log1pdx2() gives the correct values
+
+# Values of x to check:
+xvals <- c(-1.1, -1, "minL1" = -0.79149064, "-eps" = 0.01,
+           "zero" = 0, "eps" = 0.01, 1, 1.1)
+# Check all values in one call
+res1 <- suppressWarnings(log1pdx2(xvals))
+res2 <- suppressWarnings(
+  log1p(xvals) / xvals ^ 2 - 1 / (xvals * (1 + xvals))
+  )
+res2[names(xvals) == "zero"] <- 1 / 2
+test_that("log1pdx2() and log1p(x)/x^2-1/(x*(1 + x)) agree for various x", {
+  testthat::expect_equal(res1, res2)
+})
+
+# Check values individually
+
+# x < -1: expect warning and NAN result
+x <- -1.1
+test_that(paste("log1pdx2() warning for x = ", x), {
+  testthat::expect_warning(log1pdx2(x))
+})
+res <- suppressWarnings(log1pdx2(x))
+test_that(paste("log1pdx2() returns NaN for x = ", x), {
+  testthat::expect_identical(res, NaN)
+})
+
+# x = -1: expect NaN (really Inf, but direct calculation results in NaN)
+x <- -1
+res <- log1pdx2(x)
+test_that(paste("log1pdx2() returns NaN for x = ", x), {
+  testthat::expect_identical(res, NaN)
+})
+
+# x = (the default value of) minL1
+# res1 uses DPQ::logcf() and res2 uses log1px(x) / x
+# The returned values should be very close, but not identical
+x <- -0.79149064
+res1 <- log1pdx2(x)
+res2 <- log1p(x) / x ^ 2 - 1 / (x * (1 + x))
+test_that(paste("log1pdx2() and log1p(x)/x^2-1/(x*(1 + x)) agree for x =", x), {
+  testthat::expect_equal(res1, res2)
+})
+
+# x = -(the default value of) eps
+# res1 uses the explicit 5-term approximation and res2 uses DPQ::logcf()
+# res3 uses log1px(x) / x
+# The returned values should be very close, but not identical
+x <- -0.01
+res1 <- log1pdx2(x)
+res2 <- log1pdx2(x - .Machine$double.neg.eps)
+res3 <- log1p(x) / x ^ 2 - 1 / (x * (1 + x))
+test_that(paste("log1pdx2() and log1pdx2(x) using logcf() agree for x = ", x), {
+  testthat::expect_equal(res1, res2)
+})
+test_that(paste("log1pdx2() and log1p(x)/x^2-1/(x*(1 + x)) agree for x = ", x), {
+  testthat::expect_equal(res1, res3)
+})
+
+# x = 0. In the limit as x becomes 0, log1pdx() = 1
+x <- 0
+res <- log1pdx2(x)
+test_that(paste("log1pdx2() and log1pdx2(x) using logcf() agree for x = ", x), {
+  testthat::expect_identical(res, 1 / 2)
+})
+
+# x = (the default value of) eps
+# res1 uses the explicit 5-term approximation and res2 uses DPQ::logcf()
+# res3 uses log1p(x) / x ^ 2 - 1 / (x * (1 + x))
+# The returned values should be very close, but not identical
+x <- 0.01
+res1 <- log1pdx2(x)
+res2 <- log1pdx2(x + .Machine$double.neg.eps)
+res3 <- log1p(x) / x ^ 2 - 1 / (x * (1 + x))
+test_that(paste("log1pdx2() and log1pdx2(x) using logcf() agree for x = ", x), {
+  testthat::expect_equal(res1, res2)
+})
+test_that(paste("log1pdx2() and log1p(x)/x^2-1/(x*(1 + x)) agree for x = ", x), {
+  testthat::expect_equal(res1, res3)
+})
+
+# x = 1
+# res1 uses DPQ::logcf() and res2 uses log1px(x) / x
+# The returned values should be very close, but not identical
+x <- 1
+res1 <- log1pdx2(x)
+res2 <- log1p(x) / x ^ 2 - 1 / (x * (1 + x))
+test_that(paste("log1pdx2() and log1p(x)/x^2-1/(x*(1 + x)) agree for x = ", x), {
+  testthat::expect_equal(res1, res2)
+})
+
+# x > 1
+# res1 and res2 use log1px(x) / x
+# The returned values should be identical
+x <- 1.1
+res1 <- log1pdx2(x)
+res2 <- log1p(x) / x ^ 2 - 1 / (x * (1 + x))
+test_that(paste("log1pdx2() and log1p(x)/x^2-1/(x*(1 + x)) agree for x = ", x), {
+  testthat::expect_identical(res1, res2)
+})
