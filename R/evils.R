031bed4990d9bac81a4b2685d1f23b32444fe261
Paul Northrop <p.northrop@ucl.ac.uk>
2023-12-28 00:39:13 +0000

    sumR not needed

--- R/evils.R
+++ R/evils.R
@@ -2,12 +2,10 @@
 #'
 #' Provides functions to calculate the log-likelihood, score and observed
 #' information for extreme value models in cases where the shape parameter is
-#' very close to zero.  In these cases, the quantities of interest are
-#' expressed as a series expansion, whose value is approximated using the
-#' \code{\link[sumR]{sumR}} package.
+#' very close to zero.  In these cases, ...
 #'
 #' @details The main functions are
-#' See \code{vignette("introduction-to-evils", package = "evils")} for an
+#' See `vignette("introduction-to-evils", package = "evils")` for an
 #' overview of the package.
 #' @docType package
 #' @name evils
