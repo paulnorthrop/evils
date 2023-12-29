1332a040f700971538b281fc75bfec4f759363f4
Paul Northrop <p.northrop@ucl.ac.uk>
2023-12-29 21:36:57 +0000

    Tidy tests

--- tests/testthat/test-log1pdx.R
+++ tests/testthat/test-log1pdx.R
@@ -6,39 +6,13 @@ xvals <- c(-1.1, -1, "minL1" = -0.79149064, "-eps" = 0.01,
 # Check all values in one call
 res1 <- suppressWarnings(log1pdx(xvals))
 res2 <- suppressWarnings(log1p(xvals) / xvals)
+# x = 0. In the limit as x becomes 0, log1pdx() = 1
 res2[names(xvals) == "zero"] <- 1
 test_that("log1pdx() and log1p(x) / x agree for various x", {
   testthat::expect_equal(res1, res2)
 })
 
-# Check values individually
-
-# x < -1: expect warning and NAN result
-x <- -1.1
-test_that(paste("log1pdx() warning for x = ", x), {
-  testthat::expect_warning(log1pdx(x))
-})
-res <- suppressWarnings(log1pdx(x))
-test_that(paste("log1pdx() returns NaN for x = ", x), {
-  testthat::expect_identical(res, NaN)
-})
-
-# x = -1: expect Inf
-x <- -1
-res <- log1pdx(x)
-test_that(paste("log1pdx() returns NaN for x = ", x), {
-  testthat::expect_identical(res, Inf)
-})
-
-# x = (the default value of) minL1
-# res1 uses DPQ::logcf() and res2 uses log1px(x) / x
-# The returned values should be very close, but not identical
-x <- -0.79149064
-res1 <- log1pdx(x)
-res2 <- log1p(x) / x
-test_that(paste("log1pdx() and log1p(x) / x agree for x = ", x), {
-  testthat::expect_equal(res1, res2)
-})
+# Check x = -0.01 and x = 0.01 individually
 
 # x = -(the default value of) eps
 # res1 uses the explicit 5-term approximation and res2 uses DPQ::logcf()
@@ -55,13 +29,6 @@ test_that(paste("log1pdx() and log1p(x) / x agree for x = ", x), {
   testthat::expect_equal(res1, res3)
 })
 
-# x = 0. In the limit as x becomes 0, log1pdx() = 1
-x <- 0
-res <- log1pdx(x)
-test_that(paste("log1pdx() and log1pdx(x) using logcf() agree for x = ", x), {
-  testthat::expect_identical(res, 1)
-})
-
 # x = (the default value of) eps
 # res1 uses the explicit 5-term approximation and res2 uses DPQ::logcf()
 # res3 uses log1px(x) / x
@@ -75,24 +42,4 @@ test_that(paste("log1pdx() and log1pdx(x) using logcf() agree for x = ", x), {
 })
 test_that(paste("log1pdx() and log1p(x) / x agree for x = ", x), {
   testthat::expect_equal(res1, res3)
-})
-
-# x = 1
-# res1 uses DPQ::logcf() and res2 uses log1px(x) / x
-# The returned values should be very close, but not identical
-x <- 1
-res1 <- log1pdx(x)
-res2 <- log1p(x) / x
-test_that(paste("log1pdx() and log1p(x) / x agree for x = ", x), {
-  testthat::expect_equal(res1, res2)
-})
-
-# x > 1
-# res1 and res2 use log1px(x) / x
-# The returned values should be identical
-x <- 1.1
-res1 <- log1pdx(x)
-res2 <- log1p(x) / x
-test_that(paste("log1pdx() and log1p(x) / x agree for x = ", x), {
-  testthat::expect_identical(res1, res2)
 })
