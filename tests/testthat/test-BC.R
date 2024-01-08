af82823a7307cfda0d23f1b0b26c6564eaceb0a1
Paul Northrop <p.northrop@ucl.ac.uk>
2024-01-08 22:18:10 +0000

    Tests of BC special cases

---
+++ tests/testthat/test-BC.R
@@ -0,0 +1,25 @@
+# Some basic checks of the Box-Cox transformation function BC()
+
+test_that("BC(x), x < 0 errors", {
+  expect_error(BC(-1, lambda = 0))
+})
+
+test_that("BC(x = NA) returns NA", {
+  expect_equal(BC(c(1, NA), lambda = 0), c(log(1), NA))
+})
+
+test_that("BC(lambda = NA) returns NA", {
+  expect_equal(BC(x = 2, lambda = c(0, NA)), c(log(2), NA))
+})
+
+test_that("BC(x = Inf, lambda < 0) returns -1/lambda", {
+  expect_equal(BC(x = Inf, lambda = -2), 1 / 2)
+})
+
+test_that("BC(x = 0, lambda < 0) returns -Inf", {
+  expect_equal(BC(x = 0, lambda = -2), -Inf)
+})
+
+test_that("BC(x = (Inf, 0), lambda < 0) returns (1/2, -Inf)", {
+  expect_equal(BC(x = c(Inf, 0), lambda = -2), c(1 / 2, -Inf))
+})
